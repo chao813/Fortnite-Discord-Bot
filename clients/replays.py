46b30d378d80f17b8e6b64e4d06eaeaac34782e3
Yuan Chao <chaoo.yuan@gmail.com>
2021-04-15 23:04:56 -0400

    Read replay files (#20)
    * add replay commands
    
    * update eliminated_me_dict in replays.py
    
    * fix issue with showing who eliminated us
    
    * add replay connection to ftps server

---
+++ clients/replays.py
@@ -0,0 +1,51 @@
+import os
+import ast
+import glob
+
+import utils.ftps as ftps
+from ray import Reader
+
+SQUAD_PLAYERS_GUID_DICT = ast.literal_eval(str(os.getenv("SQUAD_PLAYERS_GUID_DICT")))
+REPLAY_FILE_PATH = os.getenv("REPLAY_FILE_PATH")
+
+def eliminated_me(elim, eliminated_me_dict):
+    if elim.eliminated.guid in SQUAD_PLAYERS_GUID_DICT and not elim.knocked:
+        if elim.eliminator.guid in eliminated_me_dict:
+            eliminated_me_dict[elim.eliminator.guid].append(SQUAD_PLAYERS_GUID_DICT[elim.eliminated.guid])
+        else:
+            eliminated_me_dict[elim.eliminator.guid] = [SQUAD_PLAYERS_GUID_DICT[elim.eliminated.guid]]
+    return eliminated_me_dict
+
+def eliminated_by_me(elim, eliminated_by_me_dict):
+    if elim.eliminator.guid in SQUAD_PLAYERS_GUID_DICT and not elim.knocked:
+        if elim.eliminated.guid in eliminated_by_me_dict:
+            eliminated_by_me_dict[SQUAD_PLAYERS_GUID_DICT[elim.eliminator.guid]].append(elim.eliminated.guid)
+        else:
+            eliminated_by_me_dict[SQUAD_PLAYERS_GUID_DICT[elim.eliminator.guid]] = [elim.eliminated.guid]
+    return eliminated_by_me_dict
+
+def generate_eliminations_dict(eliminations, who_elim_who_func):
+    eliminations_dict = {}
+
+    for elim in eliminations:
+        eliminations_dict = who_elim_who_func(elim, eliminations_dict)
+
+    return eliminations_dict
+                    
+
+def process_replays(): 
+    try:
+        connected_ftps = ftps.connect_to_ftp()
+        latest_replay_file = ftps.download_file_from_ftp(connected_ftps)
+        with Reader(latest_replay_file.name) as replay:
+            eliminated_me_dict = generate_eliminations_dict(replay.eliminations, eliminated_me)
+            eliminated_by_me_dict = generate_eliminations_dict(replay.eliminations, eliminated_by_me)
+            delete_replay_file(latest_replay_file.name)
+            return eliminated_me_dict, eliminated_by_me_dict
+    except:
+        print("No replay file found")
+        return None, None
+
+
+def delete_replay_file(replay_file):
+    os.remove(replay_file)
---
+++ utils/ftps.py
@@ -0,0 +1,41 @@
+import os
+import glob
+from ftplib import FTP_TLS
+
+FTPS_HOST = os.getenv("FTPS_HOST")
+FTPS_PORT = int(os.getenv("FTPS_PORT"))
+FTPS_USER = os.getenv("FTPS_USER")
+FTPS_PASSWORD = os.getenv("FTPS_PASSWORD")
+
+FTPS_REPLAY_FILE_PATH = os.getenv("FTPS_REPLAY_FILE_PATH")
+DOWNLOADED_REPLAY_FILE_PATH = os.getenv("DOWNLOADED_REPLAY_FILE_PATH")
+
+def connect_to_ftp():
+    ftps = FTP_TLS()
+    ftps.connect(FTPS_HOST, FTPS_PORT)
+    ftps.login(user=FTPS_USER, passwd = FTPS_PASSWORD)
+    return ftps
+
+
+def download_file_from_ftp(ftps):
+    ftps.cwd(FTPS_REPLAY_FILE_PATH)
+
+    list_of_replay_files = list(ftps.mlsd())
+    list_of_replay_files.sort(key = lambda replay: replay[1]['modify'], reverse = True)
+    latest_replay_file = list_of_replay_files[0][0]
+
+    saved_replay_file = open(DOWNLOADED_REPLAY_FILE_PATH + latest_replay_file, 'wb')
+    ftps.retrbinary('RETR ' + latest_replay_file, saved_replay_file.write, 1024)
+
+    delete_file_from_ftp(ftps, latest_replay_file)
+    ftps.quit()
+    saved_replay_file.close()
+
+    return saved_replay_file
+
+
+def delete_file_from_ftp(ftps, latest_replay_file):
+    ftps.delete(FTPS_REPLAY_FILE_PATH + latest_replay_file)
+
+
+
