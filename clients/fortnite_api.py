ee14d9f6a50773a9568375b58b192d5504e88010
chao813 <chaoo.yuan@gmail.com>
2021-05-05 21:15:10 -0400

    refactor replay elim function

--- clients/fortnite_api.py
+++ clients/fortnite_api.py
@@ -35,7 +35,8 @@ async def get_player_stats(ctx, player_name, guid):
             result = await _get_player_account_id(session, player_name, "psn")
             stats = await _get_player_stats(session, account_id)
             if stats["global_stats"] is None:
-                await ctx.send("{player_name}'s statistics are hidden".format(player_name=stats["name"]))
+                stats_hidden_embed=discord.Embed(title="{player_name}'s statistics are hidden".format(player_name=stats["name"]))
+                await ctx.send(embed=stats_hidden_embed)
                 return
 
         username = stats["name"]
