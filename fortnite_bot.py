fb9b5e353e8d9f42a2f754a8d494e82bf5da32f8
chao813 <chaoo.yuan@gmail.com>
2021-05-06 23:16:03 -0400

    add examples to commands, update replay elims by player

--- fortnite_bot.py
+++ fortnite_bot.py
@@ -95,7 +95,7 @@ async def on_guild_join(guild):
 @bot.event
 async def on_voice_state_update(member, before, after):
     """ Event handler to track squad stats on voice channel join """
-    if interactions.update_current_squad_player_list(member, before, after):
+    if interactions.send_player_to_squad_player_list(member, before, after):
         if member.display_name in FORTNITE_DISCORD_ROLE_USERS_DICT:
             if FORTNITE_DISCORD_ROLE_USERS_DICT[member.display_name] not in SQUAD_PLAYERS_LIST:
                 SQUAD_PLAYERS_LIST.append(FORTNITE_DISCORD_ROLE_USERS_DICT[member.display_name])
@@ -294,8 +294,12 @@ async def replays_operations(ctx, *params):
         if not command:
             logger.info("Outputting players that eliminated us")
             await output_replay_eliminated_me_stats_message(ctx, eliminated_me_dict, username, silent=False)
-        else:
+        elif command != commands.REPLAYS_COMMAND and \
+                command not in commands.REPLAYS_ELIMINATED_COMMANDS and \
+                command not in commands.REPLAYS_LOG_COMMANDS:
             await ctx.send(f"Command provided '{command}' is not valid")
+        else:
+            await ctx.send(f"{command} left the channel")
 
 
 async def output_replay_eliminated_me_stats_message(ctx, eliminated_me_dict, username, silent):
