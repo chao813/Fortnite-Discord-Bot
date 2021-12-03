350127474edd06a93b1fa18e4e6ca3c06d6d2e6d
Kevin Lin <kwkevinlin@users.noreply.github.com>
2021-12-02 18:10:01 -0800

    Mid-progress refactor

--- database/mysql.py
+++ database/mysql.py
@@ -69,11 +69,43 @@ class MySQL:
         }
         return await self._fetch_all(query, params)
 
-    async def _executemany(self, query, params=None):
+    async def fetch_game_record(self, params):
+        """"""""
+        query = """SELECT *
+                   FROM games
+                   WHERE discord_chat_id = %(discord_chat_id)s
+                   ORDER BY id desc
+                   LIMIT 1;
+                """
+        return await self._fetch_one(query, params)
+
+    async def insert_game_record(self, params):
+        """"""
+        query = """INSERT INTO games (`discord_chat_id`, `start_date`, `end_date`,
+                                      `placement`, `total_kills`, `eliminations`,
+                                      `eliminated_by`)
+                   VALUES (%(discord_chat_id)s, %(start_date)s, %(end_date)s,
+                           %(total_kills)s, %(eliminated)s, %(eliminated_by)s);
+                """
+        await self._execute(query, params)
+
+    async def _execute(self, query, params=None):
         """ Execute SQL query """
+        async with self._conn.cursor() as cursor:
+            await cursor.execute(query, params)
+            await self._conn.commit()
+
+    async def _executemany(self, query, params=None):
+        """ Execute many SQL query parameters """
         async with self._conn.cursor() as cursor:
             await cursor.executemany(query, params)
             await self._conn.commit()
+
+    async def _fetch_one(self, query, params=None):
+        """ Fetch row from MySQL """
+        async with self._conn.cursor() as cursor:
+            await cursor.execute(query, params)
+            return await cursor.fetchone()
 
     async def _fetch_all(self, query, params=None):
         """ Fetch rows from MySQL """
