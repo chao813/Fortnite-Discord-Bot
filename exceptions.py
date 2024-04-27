ecd877c5f3d9dc1824bfe8e343a609b17dc51e38
Kevin Lin <kwkevinlin@users.noreply.github.com>
2024-04-27 13:54:26 -0700

    Misc changes on rank, track, and maps (#76)
    * Misc changes
    
    * Add rank to DB insert query

--- exceptions.py
+++ exceptions.py
@@ -8,3 +8,7 @@ class UserStatisticsNotFound(Exception):
 
 class NoSeasonDataError(Exception):
     """ No season data found """
+
+
+class NoRankedDataError(Exception):
+    """ No ranked data found """
